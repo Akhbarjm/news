7b9da6eea4a30dc75e513fe92db282eae7d8af31
Akhbarjm <rezaforext@gmail.com>
2025-03-21 03:28:07 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # config.py
-BOT_TOKEN = "8081419193:AAGlc0G1Vrza85004rSgXMmCR8-EYCKbQ8A"  # توکن رباتت رو از BotFather بگیر
-MASTER_ADMIN_ID = 6704680236  # آیدی تلگرامت رو اینجا بذار
-MASTER_PASSWORD = "یاعلی"  # کلمه رمز برای ثبت مستر ادمین اولیه
-CHECK_CHANNEL = "-1001954722831"  # آیدی کانال خصوصی برای چک کردن ادمین‌ها
-BACKUP_CHAT_ID = "6704680236"  # آیدی خودت برای بکاپ (نه کانال)
+BOT_TOKEN = "8081419193:AAGlc0G1Vrza85004rSgXMmCR8-EYCKbQ8A"
+MASTER_ADMIN_ID = 6704680236  # آیدی تلگرامت
+MASTER_PASSWORD = "یاعلی"
+BACKUP_CHAT_ID = "6704680236"
